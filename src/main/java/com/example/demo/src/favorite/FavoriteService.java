c071753747ffae172f40d9d6fb85d89c3982c121
KiBeomChung <kibeom5118@naver.com>
2022-07-20 18:08:22 +0900

    상점 팔로우 API 추가 (jwt, validation 적용 완료)

--- src/main/java/com/example/demo/src/favorite/FavoriteService.java
+++ src/main/java/com/example/demo/src/favorite/FavoriteService.java
@@ -1,11 +1,14 @@
 package com.example.demo.src.favorite;
 
+import com.example.demo.config.BaseException;
 import com.example.demo.src.favorite.model.PostFavoriteStoreRes;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.example.demo.config.BaseResponseStatus.NOT_AVALIABLE_ADD_FOLLOW;
 
 @Service
 public class FavoriteService {
@@ -23,14 +26,18 @@ public class FavoriteService {
         this.jwtService = jwtService;
     }
 
-    public PostFavoriteStoreRes addFavorite(int followingId, int followerId){
+    public PostFavoriteStoreRes addFavorite(int followingId, int followerId) throws BaseException {
+
+        if(favoriteDao.checkUserStatus(followingId) == 0){ // 해당 상점이 신고 받거나 탈퇴한 상점이 아닌지 확인
+            throw new BaseException(NOT_AVALIABLE_ADD_FOLLOW);
+        }
 
         String lastInsertIdStr = favoriteDao.addFollowing(followingId);
-        System.out.println("lastInsertIdStr : " + lastInsertIdStr);
+        //     System.out.println("lastInsertIdStr : " + lastInsertIdStr);
         int lastInsertId = Integer.parseInt(lastInsertIdStr);
-        System.out.println("lastInsertId : " + lastInsertId);
+        //     System.out.println("lastInsertId : " + lastInsertId);
         String result = favoriteDao.receiveFollowing(followerId, lastInsertId);
-        System.out.println("Service Result : " + result);
+        //     System.out.println("Service Result : " + result);
 
         return new PostFavoriteStoreRes(result);
     }
