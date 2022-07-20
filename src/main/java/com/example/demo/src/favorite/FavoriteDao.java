e5c3cac2a47644ff00c03e1e7108521e112d9cb7
KiBeomChung <kibeom5118@naver.com>
2022-07-21 05:12:28 +0900

    내가 팔로우 하는 상점 목록 조회 API (이미지 처리 쿼리 에러 해결x)

--- src/main/java/com/example/demo/src/favorite/FavoriteDao.java
+++ src/main/java/com/example/demo/src/favorite/FavoriteDao.java
@@ -1,10 +1,13 @@
 package com.example.demo.src.favorite;
 
+import com.example.demo.src.favorite.model.GetFavoriteUserDetailRes;
+import com.example.demo.src.favorite.model.GetFavoriteUserProductsDetailRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class FavoriteDao {
@@ -16,7 +19,7 @@ public class FavoriteDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public String addFollowing(int followingId){
+    public String addFollowing(int followingId) {
         String addFollwingQuery = "insert into Following(followingId) values (?)"; // ? 를 팔로잉 하는것
         int addFollowingParam = followingId;
         this.jdbcTemplate.update(addFollwingQuery, addFollowingParam);
@@ -25,8 +28,8 @@ public class FavoriteDao {
         return this.jdbcTemplate.queryForObject(lastInsertIdQuery, String.class);
     }
 
-    public String receiveFollowing(int followerId, int lastInsertId){
-        String receiveFollowingQuery =  "insert into Follow(followerId, followingId) values(?, ?)";
+    public String receiveFollowing(int followerId, int lastInsertId) {
+        String receiveFollowingQuery = "insert into Follow(followerId, followingId) values(?, ?)";
         Object[] reveiveFollowingParams = new Object[]{followerId, lastInsertId};
         this.jdbcTemplate.update(receiveFollowingQuery, reveiveFollowingParams);
 
@@ -34,7 +37,7 @@ public class FavoriteDao {
         return this.jdbcTemplate.queryForObject(lastInsertIdQuery, String.class);
     }
 
-    public int deleteFavorite(int followingId, int followerId){
+    public int deleteFavorite(int followingId, int followerId) {
         String deleteFollowingQuery = "delete F, FI\n" +
                 "from Follow F inner join Following FI\n" +
                 "where F.followingId = FI.id\n" +
@@ -44,10 +47,67 @@ public class FavoriteDao {
         return this.jdbcTemplate.update(deleteFollowingQuery, deleteFollowingParams);
     }
 
-    public int checkUserStatus(int followingId){ //자신이 팔로우 하려는 상점이 신고 받지 않았는지 체크 하는 메소드
+    public int checkUserStatus(int followingId) { //자신이 팔로우 하려는 상점이 신고 받지 않았는지 체크 하는 메소드
         String checkUserStatusQuery = "select exists(select id from Users where Users.id = ? AND Users.status ='NORMAL')";
         int checkUserStatusParam = followingId;
         return this.jdbcTemplate.queryForObject(checkUserStatusQuery, int.class, checkUserStatusParam);
     }
 
+    public List<GetFavoriteUserDetailRes> getFavoriteUserDetailList(int userId) {
+        String getFavoriteUserDetailQuery = "select Users.ImageUrl, Users.storeName, count(distinct Following.id) as productsNum, count(distinct Products.productId) as followerNum\n" +
+                "from Users\n" +
+                "inner join Following on Users.id = Following.followingId\n" +
+                "left join Products on Products.userId = Users.id\n" +
+                "inner join Follow on Follow.FollowingId = Following.id\n" +
+                "where Following.followingId in (\n" +
+                "    select Following.followingId\n" +
+                "from Following\n" +
+                "inner join Follow on Follow.followingId = Following.id\n" +
+                "where Follow.followerId = ?\n" +
+                "      )\n" +
+                "group by storeName";
+        String getFavoriteUserProductsDetailQuery = "select distinct ProductImages.imageUrl, Products.price\n" +
+                "from ProductImages\n" +
+                "inner join Products on Products.productId = ProductImages.productId\n" +
+                "inner Join Following on Following.followingId = Products.userId\n" +
+                "where Following.followingId in (\n" +
+                "    select Following.followingId\n" +
+                "    from Following\n" +
+                "    inner join Follow on Follow.followingId = Following.id\n" +
+                "where Follow.followerId = ?)";
+        int getFavoriteUserDetailParam = userId;
+        int getFavoriteUserProductsDetailParam = userId;
+        return this.jdbcTemplate.query(getFavoriteUserDetailQuery,
+                (rs, rowNum) -> new GetFavoriteUserDetailRes(
+                        rs.getString("imageUrl"),
+                        rs.getString("storeName"),
+                        rs.getInt("productsNum"),
+                        rs.getInt("followerNum"),
+                        this.jdbcTemplate.query(getFavoriteUserProductsDetailQuery,
+                                (rs1, rowNum1) -> new GetFavoriteUserProductsDetailRes(
+                                        rs1.getString("imageUrl"),
+                                        rs1.getInt("price")),
+                                getFavoriteUserProductsDetailParam)
+                ),
+                getFavoriteUserDetailParam);
+    }
+
+//    public List<GetFavoriteUserProductsDetailRes> getFavoriteUserProductsDetailList(int userId) {
+//        String getFavoriteUserProductsDetailQuery = "select distinct ProductImages.imageUrl, Products.price\n" +
+//                "from ProductImages\n" +
+//                "inner join Products on Products.productId = ProductImages.productId\n" +
+//                "inner Join Following on Following.followingId = Products.userId\n" +
+//                "where Following.followingId in (\n" +
+//                "    select Following.followingId\n" +
+//                "    from Following\n" +
+//                "    inner join Follow on Follow.followingId = Following.id\n" +
+//                "where Follow.followerId = ?)";
+//        int getFavoriteUserProductsDetailParam = userId;
+//        return this.jdbcTemplate.query(getFavoriteUserProductsDetailQuery,
+//                (rs, rowNum) -> new GetFavoriteUserProductsDetailRes(
+//                        rs.getString("imageUrl"),
+//                        rs.getInt("price")),
+//                getFavoriteUserProductsDetailParam);
+//    }
 }
+
